d2cb25c499bd5bd8c2ed88d6a698a2b5801e0941
djmillan <156827081+djmillan@users.noreply.github.com>
2024-03-08 08:26:34 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,15 +20,17 @@
                 for (int i = 0; i < nums.Length; i++)
                 {
                     Console.Write($"Enter the integer for #{count + i} >> ");
-                    nums[i] = int.Parse(Console.ReadLine());
+                    nums[i] = Convert.ToInt32(Console.ReadLine());
                 }
-                Console.Write("Values: ");
+                Console.WriteLine("Values: ");
                 foreach (int i in nums)
                 {
                     Console.Write($"{i} ");
                 }
-                TwoSum(nums, target);
-                Console.WriteLine($"\n{i},{j}");
+                int[] resultIndices = TwoSum(nums, target);
+                
+                Console.WriteLine($"Output: ["+resultIndices[0]+","+resultIndices[1]+"]");
+                Console.Write("Explanation: Because nums["+resultIndices[0]+"] + nums["+resultIndices[1]+"] == "+target+", we return ["+resultIndices[0]+","+resultIndices[1]+"]");
             }
             catch (Exception ex)
             {
@@ -47,60 +49,6 @@
                     }
                 }
             }
-            // Return an array of -1 if no indices were found.
-            return new int[] { -1, -1 };
-            throw new ArgumentException("No solution.");
-        }
+        throw new ArgumentException("No solution.");
     }
 }
-/*
- * using System;
-
-public class TwoSumExample
-{
-    public static int[] TwoSum(int[] nums, int target)
-    {
-        for (int i = 0; i < nums.Length - 1; i++)
-        {
-            for (int j = i + 1; j < nums.Length; j++)
-            {
-                if (nums[i] + nums[j] == target)
-                {
-                    return new int[] { i, j };
-                }
-            }
-        }
-        // Return an array with -1 if no such indices are found
-        return new int[] { -1, -1 };
-    }
-
-    public static void Main()
-    {
-        Console.WriteLine("Enter the target sum:");
-        int targetSum = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Enter the length of the array:");
-        int length = Convert.ToInt32(Console.ReadLine());
-        int[] inputNums = new int[length];
-
-        Console.WriteLine("Enter the elements of the array:");
-        for (int i = 0; i < length; i++)
-        {
-            inputNums[i] = Convert.ToInt32(Console.ReadLine());
-        }
-
-        int[] resultIndices = TwoSum(inputNums, targetSum);
-
-        if (resultIndices[0] != -1)
-        {
-            Console.WriteLine("Indices of the numbers adding up to the target sum:");
-            Console.WriteLine("Index 1: " + (resultIndices[0] + 1)); // Adding 1 to make it more user-friendly
-            Console.WriteLine("Index 2: " + (resultIndices[1] + 1)); // Adding 1 to make it more user-friendly
-        }
-        else
-        {
-            Console.WriteLine("No such indices found that add up to the target sum.");
-        }
-    }
-}
-*/
